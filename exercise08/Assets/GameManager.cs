bc8f71306478434db4ac24a24bb464ffaef7e4f7
Watkinhj <117215234+Watkinhj@users.noreply.github.com>
2023-04-04 20:29:57 -0400

    Created the movement code for the pieces.

--- exercise08/Assets/GameManager.cs
+++ exercise08/Assets/GameManager.cs
@@ -37,5 +37,27 @@ public class GameManager : MonoBehaviour
                 }
             }
         }
+
+        // Code for movement.
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Create a ray from where the player clicked into the 3d world
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+
+                // If we get in here, it means that we hit something. 'hit' is
+                // an object that Unity filled with all of the info about what the Raycast hit.
+                //
+                // Check to make sure we actually clicked on the ground. Don't forget to
+                // create and add the tag "Ground" to your ground!
+                if (hit.collider.CompareTag("Ground"))
+                {
+                    // Set the destination to where the player clicked.
+                    selectedUnit.nma.SetDestination(hit.point);
+                }
+            }
+        }
     }
 }
--- exercise08/Assets/UnitScript.cs
+++ exercise08/Assets/UnitScript.cs
@@ -14,6 +14,7 @@ public class UnitScript : MonoBehaviour
     public bool selected = false;
 
     GameManager gm;
+    public UnityEngine.AI.NavMeshAgent nma;
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +23,18 @@ public class UnitScript : MonoBehaviour
 
         GameObject gmObj = GameObject.Find("GameManagerObject");
         gm = gmObj.GetComponent<GameManager>();
+
+        nma = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Here's the code for the movement.
+        Vector3 rayStartPos = transform.position;
+        // Raise the position up 1.5 units
+        rayStartPos.y = rayStartPos.y + 1.5f;
+        Debug.DrawRay(rayStartPos, transform.forward * 10);
     }
 
     private void OnMouseEnter()
